ba2e5e0a9288e962511f433eb80599c369370bca
Kieran Youngman <48002179+kierany5@users.noreply.github.com>
2025-06-24 22:44:44 +0100

    Load real providers in test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,5 +3,7 @@
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "ai_toolkit"
 require "ai_toolkit/providers/fake"
+require "ai_toolkit/providers/claude"
+require "ai_toolkit/providers/bedrock"
 
 require "minitest/autorun"
