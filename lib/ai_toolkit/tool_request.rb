dc55d93a5b8cec755c9ee7fe13517a903263461b
Kieran Youngman <48002179+kierany5@users.noreply.github.com>
2025-06-25 14:29:55 +0100

    Refactor result classes into standalone files

---
+++ lib/ai_toolkit/tool_request.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require_relative 'result_item'
+
+module AiToolkit
+  # Represents a tool request from the LLM.
+  class ToolRequest < ResultItem
+    attr_reader :id, :name, :input
+
+    # @param id [String]
+    # @param name [String]
+    # @param input [Object]
+    def initialize(id:, name:, input:)
+      @id = id
+      @name = name
+      @input = input
+    end
+  end
+end
